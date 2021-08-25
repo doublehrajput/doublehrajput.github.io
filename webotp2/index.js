9cdc94d0f6e566acf425c120bb8f6eda17a6f989
Harish Rajput <doublehrajput@gmail.com>
2021-08-25 14:43:54 +0530

    added

---
+++ webotp2/index.js
@@ -0,0 +1,24 @@
+if ('OTPCredential' in window) {
+    window.addEventListener('DOMContentLoaded', e => {
+        const input = document.querySelector('#otp');
+        if (!input) return;
+        const ac = new AbortController();
+        navigator.credentials.get({
+            otp: { transport: ['sms'] },
+            signal: ac.signal
+        }).then(otp => {
+            alert(otp)
+            input.value = otp.code;
+        }).catch(err => {
+            try {
+                alert(typeof err)
+                alert(JSON.stringify(err))
+                alert(err);
+            } catch (error) {
+                alert(err)
+            }
+        });
+    });
+} else {
+    alert("OTPCredential is not window")
+}
